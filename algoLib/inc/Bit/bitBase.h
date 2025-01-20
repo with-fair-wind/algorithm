7f1418bca1602c01f04b2d71bb994ba8a6983a56
kk <yangyukai@zwcad.com>
2025-01-20 17:59:44 +0800

    add printBinary

---
+++ algoLib/inc/Bit/bitBase.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <climits>
+
+namespace BitSystem
+{
+    template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+    void printBinary(T num)
+    {
+        for (int i = sizeof(T) * CHAR_BIT - 1; i >= 0; --i)
+            std::cout << ((num & (static_cast<T>(1) << i)) == 0 ? "0" : "1");
+        std::cout << std::endl;
+    }
+}
--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,7 +1,9 @@
 #include <GiniCal/experiment.h>
+#include <Bit/bitBase.h>
 
 int main()
 {
-    GiniCal::experiment(100, 1000);
+    // GiniCal::experiment(100, 1000);
+    BitSystem::printBinary(4);
     return 0;
 }
