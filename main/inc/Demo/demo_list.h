e2c53d39d00a4e62a276a0039cb62f77faa1c549
kk <921232958@qq.com>
2025-01-22 16:24:41 +0800

    add list and test shared_ptr && thread

---
+++ main/inc/Demo/demo_list.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <List/list.h>
+
+namespace Demo_List
+{
+    void demo_v1();
+    void demo_v2();
+} // namespace Demo_List
--- main/src/main.cpp
+++ main/src/main.cpp
@@ -2,6 +2,9 @@
 #include <Demo/demo_bit.h>
 #include <Demo/demo_ginical.h>
 #include <Demo/demo_binary_search.h>
+#include <Demo/demo_list.h>
+
+#include <Test/test_thread_combine_sharedptr.h>
 
 int main()
 {
@@ -9,7 +12,10 @@ int main()
     // Demo_Sort::demo_v2();
     // Demo_BitSystem::demo_v1();
     // Demo_GiniCal::demo_v1();
-    Demo_BinarySearch::demo_v1();
+    // Demo_BinarySearch::demo_v1();
+    // Demo_List::demo_v1();
+    Demo_List::demo_v2();
+    // Test_Thread_Combine_SharedPtr::test_v1();
 
     return 0;
 }
